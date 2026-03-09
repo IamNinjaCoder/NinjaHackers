f09e438288fd8b1767986c26233d460b8baaa4bd
IamNinjaCoder <bcodingninja@gmail.com>
2026-03-09 11:21:21 +0530

    🧹 Remove test/log files from repo and update .gitignore

--- test-api.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch'); // or axios, or native fetch in node 18+
-fetch('http://localhost:3000/api/student/forgot-password', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ email: 'codingwarriorhu@gmail.com' })
-}).then(res => res.json()).then(console.log).catch(console.error);
--- test-coupon.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-const http = require('http');
-
-async function runTest() {
-  console.log('Testing coupon purchase flow...');
-  // Since we require session, we should use supertest or just trust manual verification.
-  console.log('Done.');
-}
-runTest();
